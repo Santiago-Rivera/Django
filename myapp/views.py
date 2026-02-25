f887257375e3e8fa1b518b1c30bf34ad3004bf09
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-02-25 15:48:54 -0500

    Add HTML templates for index, about, projects, and tasks pages; update views and URL routing

--- myapp/views.py
+++ myapp/views.py
@@ -1,22 +1,21 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .models import Project, Task
 from django.shortcuts import get_object_or_404
-
+from django.shortcuts import render
 # Create your views here.
 def index(request):
-    return HttpResponse("Index page")
-
-def hello(request, id):
-    print(id)
-    return HttpResponse("<h1>Hello %s</h1>" % id)
+    return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse("<h2>About Page</h2>")
+    return render(request, 'about.html')
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def projects(request):
     projects = Project.objects.all()
-    return JsonResponse({"projects": list(projects.values())})
+    return render(request, 'projects.html')
 
-def tasks(request, title):
-    task = Task.objects.get(title=title)
-    return HttpResponse("task: %s" % task.title)
+def tasks(request):
+    #task = Task.objects.get(title=title)
+    return render(request, 'tasks.html')
