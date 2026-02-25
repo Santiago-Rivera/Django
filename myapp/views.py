312745125f598c974d4bdb03f40cb24584b89758
Santiago Rivera <santiagodeveloperfullstack@outlook.com>
2026-02-25 16:00:44 -0500

    Update templates and views to pass dynamic data for title and username

--- myapp/views.py
+++ myapp/views.py
@@ -2,19 +2,30 @@ from django.http import HttpResponse
 from .models import Project, Task
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+
 # Create your views here.
+
 def index(request):
-    return render(request, 'index.html')
+    title = 'Django Course'
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 def about(request):
-    return render(request, 'about.html')
+    username = 'Santiago'
+    return render(request, 'about.html', {
+        'username': username
+    })
 
 def hello(request, username):
     return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def projects(request):
+    #projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, 'projects.html')
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
 
 def tasks(request):
     #task = Task.objects.get(title=title)
